a38074141d73c43e0d752550b646258060924846
Axies <e.liscio@tudelft.nl>
2022-08-30 15:19:45 +0200

    Add deployment configurations.

---
+++ samples/three_images/covfee.deployment.config.py
@@ -0,0 +1,6 @@
+BASE_URL = 'http://axies.ewi.tudelft.nl/covfee'
+APP_PORT = 80
+
+PROJECT_WWW_URL = 'http://axies.ewi.tudelft.nl/covfee-www'
+BUNDLE_URL = 'http://axies.ewi.tudelft.nl/covfee-www'
+SERVE_WWW = False
